0ea9a4d4b5b71a41a9635b86a4940b15488fd50f
antonio <antonioborgeslima@outlook.com>
2024-01-19 14:11:23 -0300

    Adding Items to Suspicious Sand

--- src/main/java/net/antonio/tutorialmod/datagen/ModGlobalLootModifiersProvider.java
+++ src/main/java/net/antonio/tutorialmod/datagen/ModGlobalLootModifiersProvider.java
@@ -3,6 +3,7 @@ package net.antonio.tutorialmod.datagen;
 import net.antonio.tutorialmod.TutorialMod;
 import net.antonio.tutorialmod.item.ModItems;
 import net.antonio.tutorialmod.loot.AddItemModifier;
+import net.antonio.tutorialmod.loot.AddSusSandtemModifier;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Blocks;
@@ -28,6 +29,9 @@ public class ModGlobalLootModifiersProvider extends GlobalLootModifierProvider {
 
         add("metal_detector_from_jungle_temples", new AddItemModifier(new LootItemCondition[] {
                 new LootTableIdCondition.Builder(new ResourceLocation("chests/jungle_temple")).build() }, ModItems.METAL_DETECTOR.get()));
+
+        add("metal_detector_from_suspicious_sand", new AddSusSandtemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("archaeology/desert_pyramid")).build() }, ModItems.METAL_DETECTOR.get()));
 
     }
 }
