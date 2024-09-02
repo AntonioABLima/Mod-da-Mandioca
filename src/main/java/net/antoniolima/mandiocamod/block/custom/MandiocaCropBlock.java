0721d123ee3c6dda33b0bb84dd9375d48a03e342
antonio <antonioborgeslima@outlook.com>
2024-09-01 22:35:58 -0300

    Nova block que para mandiocas com curvas [WIP : Fazer estas box rotacionarem comforme a orientação aleatória ] Rotação de das block boxs dos estagios

--- src/main/java/net/antoniolima/mandiocamod/block/custom/MandiocaCropBlock.java
+++ src/main/java/net/antoniolima/mandiocamod/block/custom/MandiocaCropBlock.java
@@ -11,7 +11,6 @@ import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
-import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.*;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockState;
@@ -23,85 +22,135 @@ import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
+import net.minecraftforge.common.ForgeHooks;
 import org.jetbrains.annotations.NotNull;
 
 public class MandiocaCropBlock extends CropBlock implements BonemealableBlock {
     public static final int INITIAL_STAGE = 0; // Novo estágio inicial
     public static final int MAX_AGE = 5;
+    public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;;
+    public static final IntegerProperty AGE = IntegerProperty.create("age", INITIAL_STAGE, 5);
 
-    public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
-
+    protected static final VoxelShape ESTAGE_0 = Block.box(7.25, 0, 7.25, 8.75, 2, 8.75);
+    protected static final VoxelShape ESTAGE_1 =  Block.box(7.625, 2, 7.625, 8.375, 4.125, 8.375);
+    protected static final VoxelShape ESTAGE_2 =  Block.box(7.5, 2, 7.5, 8.5, 5.875, 8.5);
+    protected static final VoxelShape ESTAGE_3_NORTH = Shapes.or(
+            Block.box(7.375, 2, 7.375, 8.625, 6, 8.625),
+            Block.box(8.375, 6, 6.375, 9.625, 10.125, 7.625)
+    );
+    protected static final VoxelShape ESTAGE_3_EAST = Shapes.or(
+            Block.box(7.375, 2, 7.375, 8.625, 6, 8.625),
+            Block.box(8.375, 6, 6.375 + 2, 9.625, 10.125, 7.625 + 2)
+    );
+    protected static final VoxelShape ESTAGE_3_SOUTH = Shapes.or(
+            Block.box(7.375, 2, 7.375, 8.625, 6, 8.625),
+            Block.box(8.375 - 2, 6, 6.375 + 2, 9.625 - 2, 10.125, 7.625 + 2)
+    );
+    protected static final VoxelShape ESTAGE_3_WEST = Shapes.or(
+            Block.box(7.375, 2, 7.375, 8.625, 6, 8.625),
+            Block.box(8.375 - 2, 6, 6.375, 9.625 - 2, 10.125, 7.625)
+    );
+    protected static final VoxelShape  ESTAGE_4_NORTH = Shapes.or(
+            Block.box(7.375, 2, 7.375, 8.625, 6, 8.625),
+            Block.box(8.375, 6, 6.375, 9.625, 11, 7.625),
+            Block.box(9.375, 11, 6.375, 10.625, 17.125, 7.625)
+    );
+    protected static final VoxelShape ESTAGE_4_EAST = Shapes.or(
+            Block.box(7.375, 2, 7.375, 8.625, 6, 8.625),
+            Block.box(8.375, 6, 6.375 + 2, 9.625, 11, 7.625 + 2),
+            Block.box(9.375 - 1, 11 , 6.375 + 3, 10.625 - 1, 17.125, 7.625 + 3)
+    );
+    protected static final VoxelShape ESTAGE_4_SOUTH = Shapes.or(
+            Block.box(7.375, 2, 7.375, 8.625, 6, 8.625),
+            Block.box(8.375 - 2, 6, 6.375 + 2, 9.625 - 2, 11, 7.625 + 2),
+            Block.box(9.375 - 4, 11, 6.375 + 2, 10.625 - 4, 17.125, 7.625 + 2)
+    );
+    protected static final VoxelShape ESTAGE_4_WEST = Shapes.or(
+            Block.box(7.375, 2, 7.375, 8.625, 6, 8.625),
+            Block.box(8.375 - 2, 6, 6.375, 9.625 - 2, 11, 7.625),
+            Block.box(9.375 - 3, 11, 6.375 - 1, 10.625 - 3, 17.125, 7.625 - 1)
+    );
+    protected static final VoxelShape ESTAGE_5_NORTH = Shapes.or(
+            Block.box(7.375, 2, 7.375, 8.625, 6, 8.625),
+            Block.box(8.375, 6, 7.375, 9.625, 29.125, 8.625)
+    );
+    protected static final VoxelShape ESTAGE_5_EAST = Shapes.or(
+            Block.box(7.375, 2, 7.375, 8.625, 6, 8.625),
+            Block.box(6.375 + 1, 6, 7.375 + 1, 7.625 + 1, 29.125, 8.625 + 1)
+    );
+    protected static final VoxelShape ESTAGE_5_SOUTH = Shapes.or(
+            Block.box(7.375, 2, 7.375, 8.625, 6, 8.625),
+            Block.box(8.375 -2, 6, 7.375, 9.625 - 2, 29.125, 8.625)
+    );
+    protected static final VoxelShape ESTAGE_5_WEST = Shapes.or(
+            Block.box(7.375, 2, 7.375, 8.625, 6, 8.625),
+            Block.box(8.375 - 1, 6, 7.375 - 1, 9.625 - 1, 29.125, 8.625 - 1)
+    );
 
 
     public MandiocaCropBlock(Properties pProperties) {
         super(pProperties);
-
-//        this.registerDefaultState(this.stateDefinition.any().setValue(AGE, INITIAL_STAGE).setValue(FACING, Direction.SOUTH));
     }
 
-    public void chandeWhereIsFacing(BlockState state, Level world, BlockPos pos, BlockState oldState, boolean isMoving){
+    public void changeWhereIsFacing(BlockState state, Level world, BlockPos pos){
         if (!world.isClientSide) {
-            // Escolher uma direção aleatória ou específica
             Direction chosenDirection = Direction.Plane.HORIZONTAL.getRandomDirection(world.getRandom());
-
-            // Atualizar o estado do bloco com a nova direção
             BlockState newState = state.setValue(FACING, chosenDirection);
-
-            // Colocar o bloco com o novo estado
             world.setBlock(pos, newState, 2);
-
             System.out.println("O bloco Mandioca foi colocado no mundo virado para: " + chosenDirection);
         }
     }
-//    @Override
-//    public void onPlace(BlockState state, Level world, BlockPos pos, BlockState oldState, boolean isMoving) {
-////        super.onPlace(state, world, pos, oldState, isMoving);
-//
-//        // Verifique se está no lado do servidor para evitar que o código seja executado duas vezes (lado cliente e servidor)
-//        if (!world.isClientSide) {
-//            // Escolher uma direção aleatória ou específica
-//            Direction chosenDirection = Direction.Plane.HORIZONTAL.getRandomDirection(world.getRandom());
-//
-//            // Atualizar o estado do bloco com a nova direção
-//            BlockState newState = state.setValue(FACING, chosenDirection);
-//
-//            // Colocar o bloco com o novo estado
-////            world.setBlock(pos, newState, 2);
-//
-//            System.out.println("O bloco Mandioca foi colocado no mundo virado para: " + chosenDirection);
-//        }
-//    }
 
-    private static final VoxelShape[] SHAPE_BY_AGE = new VoxelShape[]{
-            // Estágio 0
-            Block.box(7.25, 0, 7.25, 8.75, 2, 8.75), // Novo estágio
-            // Estágio 1
-            Block.box(7.625, 2, 7.625, 8.375, 4.125, 8.375),
-            // Estágio 2
-            Block.box(7.5, 2, 7.5, 8.5, 5.875, 8.5),
-            // Estágio 3
-            Shapes.or(
-                Block.box(7.375, 2, 7.375, 8.625, 6, 8.625),
-                Block.box(8.375, 6, 6.375, 9.625, 10.125, 7.625)
-            ),
-            // Estágio 4
-            Shapes.or(
-                Block.box(7.375, 2, 7.375, 8.625, 6, 8.625),
-                Block.box(8.375, 6, 6.375, 9.625, 11, 7.625),
-                Block.box(9.375, 11, 6.375, 10.625, 17.125, 7.625)
-            ),
-            // Estágio 5
-            Shapes.or(
-                Block.box(7.375, 2, 7.375, 8.625, 6, 8.625),
-                Block.box(8.375, 6, 7.375, 9.625, 29.125, 8.625)
-            ),
+    private static final VoxelShape[] SHAPE_BY_AGE_NORTH = new VoxelShape[]{
+            ESTAGE_0,
+            ESTAGE_1,
+            ESTAGE_2,
+            ESTAGE_3_NORTH, // Estágio 3
+            ESTAGE_4_NORTH, // Estágio 4
+            ESTAGE_5_NORTH  // Estágio 5
     };
 
-    public static final IntegerProperty AGE = IntegerProperty.create("age", INITIAL_STAGE, 5);
+    private static final VoxelShape[] SHAPE_BY_AGE_EAST = new VoxelShape[]{
+            ESTAGE_0,
+            ESTAGE_1,
+            ESTAGE_2,
+            ESTAGE_3_EAST, // Estágio 3
+            ESTAGE_4_EAST, // Estágio 4
+            ESTAGE_5_EAST  // Estágio 5
+    };
+
+    private static final VoxelShape[] SHAPE_BY_AGE_SOUTH = new VoxelShape[]{
+            ESTAGE_0,
+            ESTAGE_1,
+            ESTAGE_2,
+            ESTAGE_3_SOUTH, // Estágio 3
+            ESTAGE_4_SOUTH, // Estágio 4
+            ESTAGE_5_SOUTH  // Estágio 5
+    };
+
+    private static final VoxelShape[] SHAPE_BY_AGE_WEST = new VoxelShape[]{
+            ESTAGE_0,
+            ESTAGE_1,
+            ESTAGE_2,
+            ESTAGE_3_WEST, // Estágio 3
+            ESTAGE_4_WEST, // Estágio 4
+            ESTAGE_5_WEST  // Estágio 5
+    };
 
     @Override
     public @NotNull VoxelShape getShape(@NotNull BlockState pState, @NotNull BlockGetter pLevel, @NotNull BlockPos pPos, @NotNull CollisionContext pContext) {
-        return SHAPE_BY_AGE[this.getAge(pState)];
+        int age = this.getAge(pState);
+        Direction facing =  pState.getValue(FACING);
+        switch (facing.getName()) {
+            case "east":
+                return SHAPE_BY_AGE_EAST[age];
+            case "south":
+                return SHAPE_BY_AGE_SOUTH[age];
+            case "west":
+                return  SHAPE_BY_AGE_WEST[age];
+            default:
+                return SHAPE_BY_AGE_NORTH[age];
+        }
     }
 
     @Override
@@ -112,9 +161,9 @@ public class MandiocaCropBlock extends CropBlock implements BonemealableBlock {
             int currentAge = this.getAge(pState);
             if (currentAge < this.getMaxAge()) {
                 float growthSpeed = getGrowthSpeed(this, pLevel, pPos);
-                if (net.minecraftforge.common.ForgeHooks.onCropsGrowPre(pLevel, pPos, pState, pRandom.nextInt((int)(25.0F / growthSpeed) + 1) == 0)) {
+                if (ForgeHooks.onCropsGrowPre(pLevel, pPos, pState, pRandom.nextInt((int)(25.0F / growthSpeed) + 1) == 0)) {
                     growCrops(pLevel, pPos, pState);
-                    net.minecraftforge.common.ForgeHooks.onCropsGrowPost(pLevel, pPos, pState);
+                    ForgeHooks.onCropsGrowPost(pLevel, pPos, pState);
                 }
             }
         }
@@ -178,9 +227,9 @@ public class MandiocaCropBlock extends CropBlock implements BonemealableBlock {
         } else {
             pLevel.setBlock(pPos, this.getStateForAge(nextAge), 2);
         }
-        chandeWhereIsFacing(this.getStateForAge(nextAge), pLevel, pPos, pState, false);
+        changeWhereIsFacing(this.getStateForAge(nextAge), pLevel, pPos);
     }
-//    BlockState state, Level world, BlockPos pos, BlockState oldState, boolean isMoving
+//    BlockState state, Level world, BlockPos pos
     @Override
     public int getMaxAge() {
         return  MAX_AGE;
