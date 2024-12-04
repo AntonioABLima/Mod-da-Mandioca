692ad32ed6aa9df94f35e4424bc351d33b65cdee
antonio <antonioborgeslima@outlook.com>
2024-12-04 18:03:03 -0300

    Change merchants offers ItemStack to ItemCost

--- src/main/java/net/antoniolima/mandiocamod/event/ModEvents.java
+++ src/main/java/net/antoniolima/mandiocamod/event/ModEvents.java
@@ -11,6 +11,7 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.enchantment.EnchantmentInstance;
 import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.item.trading.ItemCost;
 import net.minecraft.world.item.trading.MerchantOffer;
 import net.minecraftforge.event.village.VillagerTradesEvent;
 import net.minecraftforge.event.village.WandererTradesEvent;
@@ -30,7 +31,7 @@ public class ModEvents {
             // Level 1
             trades.get(1).add((pTrader, pRandom) -> {
                 MerchantOffer offer = new MerchantOffer(
-                        new ItemStack(Items.EMERALD, 2),
+                        new ItemCost(Items.EMERALD, 2),
                         new ItemStack(ModItems.MANDIOCA_CRUA.get(), 12),
                         10, 8, 0.1f
                 );
@@ -40,14 +41,14 @@ public class ModEvents {
 
             // Level 2
             trades.get(2).add((pTrader, pRandom) -> new MerchantOffer(
-                    new ItemStack(Items.EMERALD, 5),
+                    new ItemCost(Items.EMERALD, 5),
                     new ItemStack(ModItems.MANDIOCA_DESCASCADA.get(), 6),
                     5, 9, 0.035f)
             );
 
             // Level 3
             trades.get(3).add((pTrader, pRandom) -> new MerchantOffer(
-                    new ItemStack(Items.GOLD_INGOT, 8),
+                    new ItemCost(Items.GOLD_INGOT, 8),
                     new ItemStack(ModItems.MANDIOCA_CAULE.get(), 2),
                     2, 12, 0.075f)
             );
@@ -61,7 +62,7 @@ public class ModEvents {
         List<VillagerTrades.ItemListing> rareTrades = event.getRareTrades();
 
         genericTrades.add((pTrader, pRandom) -> new MerchantOffer(
-                new ItemStack(Items.EMERALD, 12),
+                new ItemCost(Items.EMERALD, 12),
                 new ItemStack(ModItems.MANDIOCA_CAULE.get(), 1),
                 3, 2, 0.2f));
     }
